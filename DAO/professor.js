ca6381bb942900e9cfeb4aeeb25b387120973629
Adiel Trani <117945288+adiexx@users.noreply.github.com>
2023-09-23 16:50:51 -0300

    function-insert

--- DAO/professor.js
+++ DAO/professor.js
@@ -18,3 +18,24 @@ catch(error)
     console.error(error.message);
 //caso tenha algum erro na conexao, ele sera exibido no terminal
 }
+
+//criando funções para serem excutadas no banco de dados
+
+function inserir(CPF, nome, titulacao, callback)
+{
+    connection.query(`INSERT INTO professor (CPF, nome, titulacao) VALUES (?, ?, ?)`,
+    [CPF, nome, titulacao],
+    function(err, results){
+        if(err)
+        {
+            callback(err, null );
+            // Chama o callback com erro
+        }
+        else
+        {
+            callback(null,results.affectedRows);
+            // Chama o callback com o número de linhas afetadas
+        }
+    }
+    );
+}
